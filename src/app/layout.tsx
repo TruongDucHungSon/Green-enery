5ff0b2b6575922ff77d63f7968351380c70081d5
DevSon0812 <97348196+DevSon0812@users.noreply.github.com>
2024-07-15 20:21:42 +0700

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
+import { Header } from "@/layout";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Geologica } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const geologica = Geologica({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={geologica.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,29 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+
+      screens: {
+        sm: "599.98px",
+        md: "767.98px",
+        lg: "1023.98px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
